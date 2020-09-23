d9757416bcfc9717c91b4fbe37f16f645d0615ed
Marcelo11114 <dedosearte@hotmail.com>
2020-09-23 19:01:31 -0300

    Celsios em Fahreinheit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           Double C, F; // VARIAVEIS C=centigrados F=fharenheit
+           String T;
+
+          do
+           {
+           Console.Clear();
+           Console.ForegroundColor = ConsoleColor.DarkYellow;
+           Console.WriteLine("      +---------------------+");
+           Console.WriteLine("      |                     |");
+           Console.WriteLine("      +---------------------+");
+
+           Console.SetCursorPosition(8, 1);
+           Console.ForegroundColor = ConsoleColor.Green;
+           Console.WriteLine("Graus em Fahrenheit");
+           Console.SetCursorPosition(0, 3);
+           Console.ResetColor();
+
+           Console.Write("Temperatura em Centigrados :");
+           C=Convert.ToDouble(Console.ReadLine());
+           F=C*1.8+32;
+
+           Console.WriteLine($"Temperatura em Fahrenheit é {F}°F");
+
+           Console.Write("(S)air?:");           
+           T = Console.ReadLine().ToUpper();
+           }
+
+           while(T != "S");
+
+           Console.Clear();
+
+
+
+
+        }
+    }
+}
